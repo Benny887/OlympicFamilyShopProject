b8cb74a69c8004984cb6512433b7eb372e414478
Benny <starostar92@gmail.com>
2023-02-05 00:51:52 +0700

    add category page and form

---
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/Category/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.olympicFamily.olympicFamily.BackEnd.Admin.Category;
+
+import com.olympicFamily.olympicFamily.Common.Entity.Category;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
+}
---
+++ src/test/java/com/olympicFamily/olympicFamily/CategoryRepositoryTest.java
@@ -0,0 +1,86 @@
+package com.olympicFamily.olympicFamily;
+
+import com.olympicFamily.olympicFamily.BackEnd.Admin.Category.CategoryRepository;
+import com.olympicFamily.olympicFamily.Common.Entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository repo;
+
+    @Test
+    public void testCreateRootCategory() {
+        Category category = new Category("Footwear");
+        Category savedCategory = repo.save(category);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testCreateSubCategory() {
+        Category parent = new Category(7);
+        Category subCategory = new Category("Soccer", parent);
+        Category savedCategory = repo.save(subCategory);
+
+        assertThat(savedCategory.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testGetCategory() {
+        Category category = repo.findById(2).get();
+        System.out.println(category.getName());
+
+        Set<Category> children = category.getChildren();
+
+        for (Category subCategory : children) {
+            System.out.println(subCategory.getName());
+        }
+
+        assertThat(children.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testPrintHierarchicalCategories() {
+        Iterable<Category> categories = repo.findAll();
+
+        for (Category category : categories) {
+            if (category.getParent() == null) {
+                System.out.println(category.getName());
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    System.out.println("--" + subCategory.getName());
+                    printChildren(subCategory, 1);
+                }
+            }
+        }
+    }
+
+    private void printChildren(Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for (Category subCategory : children) {
+            for (int i = 0; i < newSubLevel; i++) {
+                System.out.print("--");
+            }
+
+            System.out.println(subCategory.getName());
+
+            printChildren(subCategory, newSubLevel);
+        }
+    }
+}
