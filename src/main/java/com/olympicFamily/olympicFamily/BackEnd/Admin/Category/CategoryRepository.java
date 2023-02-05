f44f64202f94d915e273c59cd95ab819304659f4
Benny <starostar92@gmail.com>
2023-02-05 13:48:58 +0700

    add list categories

--- src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/Category/CategoryRepository.java
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/Category/CategoryRepository.java
@@ -1,8 +1,14 @@
 package com.olympicFamily.olympicFamily.BackEnd.Admin.Category;
 
 import com.olympicFamily.olympicFamily.Common.Entity.Category;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
+    @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+    public List<Category> findRootCategories();
 
 }
