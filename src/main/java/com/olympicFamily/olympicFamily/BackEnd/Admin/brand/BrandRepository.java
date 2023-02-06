29d69849a16cda79e699b0cf3f8dafe8e14ae77b
Benny <starostar92@gmail.com>
2023-02-06 22:00:17 +0700

    refactor

---
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/brand/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.olympicFamily.olympicFamily.BackEnd.Admin.brand;
+
+
+import com.olympicFamily.olympicFamily.Common.Entity.Brand;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
