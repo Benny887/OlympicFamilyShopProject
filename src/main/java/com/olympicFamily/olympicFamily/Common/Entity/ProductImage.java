5d6463f784529e8a14f521e87951d1e555b40887
Benny <starostar92@gmail.com>
2023-02-08 23:53:46 +0700

    products edit ready

--- src/main/java/com/olympicFamily/olympicFamily/Common/Entity/ProductImage.java
+++ src/main/java/com/olympicFamily/olympicFamily/Common/Entity/ProductImage.java
@@ -1,13 +1,6 @@
 package com.olympicFamily.olympicFamily.Common.Entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "product_images")
@@ -56,5 +49,9 @@ public class ProductImage {
         this.product = product;
     }
 
+    @Transient
+    public String getImagePath() {
+        return "/product-images/" + product.getId() + "/extras/" + this.name;
+    }
 
 }
