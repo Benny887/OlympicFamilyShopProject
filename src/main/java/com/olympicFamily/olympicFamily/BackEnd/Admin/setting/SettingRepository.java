539fe5565211f7db436655b9f9bddaaa5e4dae48
Benny <starostar92@gmail.com>
2023-02-17 14:30:58 +0700

    FE add currency

---
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/setting/SettingRepository.java
@@ -0,0 +1,8 @@
+package com.olympicFamily.olympicFamily.BackEnd.Admin.setting;
+
+import com.olympicFamily.olympicFamily.Common.Entity.Setting;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SettingRepository extends CrudRepository<Setting, String> {
+
+}
---
+++ src/main/java/com/olympicFamily/olympicFamily/Common/Entity/SettingCategory.java
@@ -0,0 +1,5 @@
+package com.olympicFamily.olympicFamily.Common.Entity;
+
+public enum SettingCategory {
+    GENERAL, MAIL_SERVER, MAIL_TEMPLATES, CURRENCY, PAYMENT
+}
