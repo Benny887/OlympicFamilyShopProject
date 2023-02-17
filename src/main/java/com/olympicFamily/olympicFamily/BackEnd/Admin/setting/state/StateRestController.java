a1579d65651374f1bdfad5c211c2903ec1979d56
Benny <starostar92@gmail.com>
2023-02-17 22:30:27 +0700

    add states settings

---
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/setting/state/StateRestController.java
@@ -0,0 +1,39 @@
+package com.olympicFamily.olympicFamily.BackEnd.Admin.setting.state;
+
+import com.olympicFamily.olympicFamily.Common.Entity.Country;
+import com.olympicFamily.olympicFamily.Common.Entity.State;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StateRestController {
+
+    @Autowired
+    private StateRepository repo;
+
+    @GetMapping("/states/list_by_country/{id}")
+    public List<StateDTO> listByCountry(@PathVariable("id") Integer countryId) {
+        List<State> listStates = repo.findByCountryOrderByNameAsc(new Country(countryId));
+        List<StateDTO> result = new ArrayList<>();
+
+        for (State state : listStates) {
+            result.add(new StateDTO(state.getId(), state.getName()));
+        }
+
+        return result;
+    }
+
+    @PostMapping("/states/save")
+    public String save(@RequestBody State state) {
+        State savedState = repo.save(state);
+        return String.valueOf(savedState.getId());
+    }
+
+    @GetMapping("/states/delete/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        repo.deleteById(id);
+    }
+}
