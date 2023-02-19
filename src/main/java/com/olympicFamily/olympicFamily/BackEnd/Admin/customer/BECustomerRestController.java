02ddab8ece35023dfef853651a2aa85d001b733f
Benny <starostar92@gmail.com>
2023-02-19 19:01:17 +0700

    add customer module

---
+++ src/main/java/com/olympicFamily/olympicFamily/BackEnd/Admin/customer/BECustomerRestController.java
@@ -0,0 +1,21 @@
+package com.olympicFamily.olympicFamily.BackEnd.Admin.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BECustomerRestController {
+    @Autowired
+    private BECustomerService service;
+
+    @PostMapping("/customers/check_email")
+    public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email) {
+        if (service.isEmailUnique(id, email)) {
+            return "OK";
+        } else {
+            return "Duplicated";
+        }
+    }
+}
---
+++ src/main/java/com/olympicFamily/olympicFamily/Common/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.olympicFamily.olympicFamily.Common.exception;
+
+public class CustomerNotFoundException extends Exception {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+
+}
